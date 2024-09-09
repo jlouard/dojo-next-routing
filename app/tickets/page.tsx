3a72c10de5d87029887205d34e047bb054abb44e
jlouard <julienl@hokla.com>
2024-09-09 16:51:07 +0200

    fix(tickets): solve exercice
    1

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,9 @@
+import { Tickets } from "../_components/Tickets";
+
 export default function Page() {
    return  (
       <div className="flex flex-col flex-1 items-center">
-
+         <Tickets />
       </div>
    )
 }
