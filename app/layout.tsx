251c101b4b2ebc7722b647869cf8fa3996aa82fe
jlouard <julienl@hokla.com>
2024-09-09 16:55:31 +0200

    fix(layout): add Header back

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col h-full w-full flex-1">
+        <Header />
         {children}
       </body>
     </html>
