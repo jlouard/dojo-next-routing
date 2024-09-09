752ce3eff666345bc049163cc635c15ebbcc706a
jlouard <julienl@hokla.com>
2024-09-09 17:34:36 +0200

    feat(Footer): custom footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export const Footer = () => {
     return (
-    <div className="flex justify-between w-full">
-        This is a footer
+    <div className="flex flex-col flex-1 items-center place-items-center">
+        <Image
+          src="/olympic-rings.webp"
+          alt="Paris 2024 JO Logo"
+          width={300}
+          height={200}
+          priority
+        />
     </div>
     );
 
