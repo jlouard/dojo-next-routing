9efae18faa676cf227574ad1e7eeba07e8245869
jlouard <julienl@hokla.com>
2024-09-09 18:14:06 +0200

    feat(Athletes-page): modify files to add features

--- app/(withFooter)/athletes/@sideMenu/page.tsx
+++ app/(withFooter)/athletes/@sideMenu/page.tsx
@@ -1,7 +1,14 @@
 import { SideMenu } from "@/app/_components/SideMenu";
+import { isAthleteNameSlug } from "@/app/_utils/utils";
 
-export default async function Page() {
-        return (
-            <SideMenu />
+type PageParams = {
+    params: { slugParams?: string[] };
+  };
+
+export default async function Page({ params: { slugParams } }: PageParams) {
+    const [continentCode, countryCode] = slugParams ?? [undefined, undefined];
+    if (slugParams && isAthleteNameSlug(slugParams[0] ?? "")) return null;
+    return (
+           <SideMenu continentCode={continentCode} countryCode={countryCode} />
         )
     }
--- app/_components/AthletesPage.tsx
+++ app/_components/AthletesPage.tsx
@@ -5,12 +5,22 @@ import { AthleteCard } from "./AthleteCard";
 
 type Props = {
     athletes: Athlete[],
+    countryCode?: string,
+    continentCode?: string,
 }
 
-export const AthletesPage = ({ athletes }: Props) => {
+export const AthletesPage = ({ athletes, countryCode, continentCode }: Props) => {
     let displayedAthletes = athletes;
+    if (continentCode) {   
+        displayedAthletes = athletes
+        .filter((athlete) => athlete.continentCode === continentCode);
+    }
+    if (countryCode) {   
+        displayedAthletes = athletes
+        .filter((athlete) => athlete.countryCode === countryCode);
+    }
     return (
-        <div className="flex flex-col w-full h-full">
+        <div className="flex flex-col w-full h-full p-8">
             <div className="flex flex-row gap-2 text-yellow-300 font-bold">
                 <p className="w-40">COUNTRY</p>
                 <p className="w-40">FIRST NAME</p>
